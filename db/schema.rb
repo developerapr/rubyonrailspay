61d1bf7d2718305a7d5bb4997dd7dbbe74fab817
arunendra <arunendra@gracelltech.com>
2018-11-05 16:26:10 +0530

    advac

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_102329) do
+ActiveRecord::Schema.define(version: 2018_11_05_094902) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
